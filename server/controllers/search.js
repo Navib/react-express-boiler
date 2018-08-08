ca514d117d60b405bd1a7f80a9b4e6cfb58e87aa
navib <ivan.baena.dev@gmail.com>
2018-08-07 23:05:43 -0400

    search controller working

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,15 +1,13 @@
 const Message = require('../models/message');
 
-const query = 'donald';
-
 exports.getSearchMessageQuery = function(req, res, next) {
-  Message.find({ message: { $regex: query, $options: 'ix' } }, function(
-    err,
-    messages
-  ) {
-    if (err) {
-      return next(err);
+  Message.find(
+    { message: { $regex: req.params.query, $options: 'ix' } },
+    function(err, messages) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json({ messages: messages });
     }
-    res.status(200).json({ messages: messages });
-  });
+  );
 };
